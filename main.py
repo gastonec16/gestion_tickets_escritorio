27a9f887986ff892e735d4e8569eb461f7c81da9
Gastón Cuello <gastonec16@gmail.com>
2022-10-15 01:25:24 -0300

    simplificación de código

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tkinter.font import Font
 
 #region funciones
 
-def posicionar_formulario(lista_label, lista_entry, column=0, row=1):
+def posicionar_formulario(lista_label, lista_entry, button, column=0, row=1):
     row_label = row
     row_entry = row
     for i in lista_label:
@@ -14,6 +14,8 @@ def posicionar_formulario(lista_label, lista_entry, column=0, row=1):
     for i in lista_entry:
         i.grid(column=column+1, row=row_entry, padx=(8, 24), ipady=3, sticky='we')
         row_entry += 1
+    button.grid(column=column+1, row=row_entry, padx=24, ipadx=16, ipady=6, sticky='e')
+    
 
 def registrarse():
     print('registrarse')
@@ -95,31 +97,28 @@ frame_registrarse.rowconfigure((1,2,3,4,5,6,7,8), weight=1)
 label_titulo = Label(frame_registrarse, style='titulo.TLabel', text='Registrarse en el Sistema')
 label_titulo.grid(column=0, row=0, columnspan=2)
 
-label_registrarse_nombre = Label(frame_registrarse, text='Nombre *')
-label_registrarse_apellido = Label(frame_registrarse, text='Apellido *')
-label_registrarse_legajo = Label(frame_registrarse, text='Legajo *')
-label_registrarse_email = Label(frame_registrarse, text='Email *')
-label_registrarse_usuario = Label(frame_registrarse, text='Nombre de Usuario *')
-label_registrarse_contraseña = Label(frame_registrarse, text='Contraseña *')
-label_registrarse_repetir = Label(frame_registrarse, text='Repetir Contraseña *')
-
-entry_registrarse_nombre = Entry(frame_registrarse)
-entry_registrarse_apellido = Entry(frame_registrarse)
-entry_registrarse_legajo = Entry(frame_registrarse)
-entry_registrarse_email = Entry(frame_registrarse)
-entry_registrarse_usuario = Entry(frame_registrarse)
-entry_registrarse_contraseña = Entry(frame_registrarse, show='*')
-entry_registrarse_repetir = Entry(frame_registrarse, show='*')
-
-lista_label_registrarse = [label_registrarse_nombre, label_registrarse_apellido, label_registrarse_legajo,
-label_registrarse_email, label_registrarse_usuario, label_registrarse_contraseña, label_registrarse_repetir]
-lista_entry_registrarse = [entry_registrarse_nombre, entry_registrarse_apellido, entry_registrarse_legajo,
-entry_registrarse_email, entry_registrarse_usuario, entry_registrarse_contraseña, entry_registrarse_repetir]
-
-posicionar_formulario(lista_label_registrarse, lista_entry_registrarse)
+lista_label_registrarse = [
+    Label(frame_registrarse, text='Nombre *'),
+    Label(frame_registrarse, text='Apellido *'),
+    Label(frame_registrarse, text='Legajo *'),
+    Label(frame_registrarse, text='Email *'),
+    Label(frame_registrarse, text='Nombre de Usuario *'),
+    Label(frame_registrarse, text='Contraseña *'),
+    Label(frame_registrarse, text='Repetir Contraseña *')
+]
+lista_entry_registrarse = [
+    Entry(frame_registrarse),
+    Entry(frame_registrarse),
+    Entry(frame_registrarse),
+    Entry(frame_registrarse),
+    Entry(frame_registrarse),
+    Entry(frame_registrarse, show='*'),
+    Entry(frame_registrarse, show='*')
+]
 
 button_registrarse = Button(frame_registrarse, text='Registrarse', command=registrarse, cursor='hand2')
-button_registrarse.grid(column=1, row=8, padx=24, ipadx=16, ipady=6, sticky='e')
+
+posicionar_formulario(lista_label_registrarse, lista_entry_registrarse, button_registrarse)
 
 notebook_contenido.add(frame_registrarse, text='Registrarse')
 
@@ -135,21 +134,20 @@ frame_area.rowconfigure((1,2,3,4), weight=1)
 label_titulo = Label(frame_area, style='titulo.TLabel', text='Agregar Área')
 label_titulo.grid(column=0, row=0, columnspan=2)
 
-label_area_nombre = Label(frame_area, text='Nombre de Área *')
-label_area_telefono = Label(frame_area, text='Teléfono  ')
-label_area_email = Label(frame_area, text='Email  ')
-
-entry_area_nombre = Entry(frame_area)
-entry_area_telefono = Entry(frame_area)
-entry_area_email = Entry(frame_area)
-
-lista_label_area = [label_area_nombre, label_area_telefono, label_area_email]
-lista_entry_area = [entry_area_nombre, entry_area_telefono, entry_area_email]
-
-posicionar_formulario(lista_label_area, lista_entry_area)
+lista_label_area = [
+    Label(frame_area, text='Nombre de Área *'),
+    Label(frame_area, text='Teléfono  '),
+    Label(frame_area, text='Email  ')
+]
+lista_entry_area = [
+    Entry(frame_area),
+    Entry(frame_area),
+    Entry(frame_area)
+]
 
 button_area = Button(frame_area, text='Agregar', command=agregar_area, cursor='hand2')
-button_area.grid(column=1, row=4, padx=24, ipadx=16, ipady=6, sticky='e')
+
+posicionar_formulario(lista_label_area, lista_entry_area, button_area)
 
 notebook_contenido.add(frame_area, text='Áreas')
 
@@ -165,37 +163,36 @@ frame_crear_ticket.rowconfigure((1,2,3,4,5,6,7), weight=1)
 label_titulo = Label(frame_crear_ticket, style='titulo.TLabel', text='Nuevo Ticket')
 label_titulo.grid(column=0, row=0, columnspan=2)
 
-label_crear_ticket_asunto = Label(frame_crear_ticket, text='Asunto *')
-label_crear_ticket_area = Label(frame_crear_ticket, text='Área *')
-label_crear_ticket_prioridad = Label(frame_crear_ticket, text='Prioridad *')
-label_crear_ticket_hardware = Label(frame_crear_ticket, text='Código Hardware  ')
-label_crear_ticket_tecnico = Label(frame_crear_ticket, text='Técnico  ')
-label_crear_ticket_detalle = Label(frame_crear_ticket, text='Detalle *')
-
-entry_crear_ticket_asunto = Entry(frame_crear_ticket)
-combobox_crear_ticket_area = Combobox(frame_crear_ticket,values=lista_area, state='readonly')
-combobox_crear_ticket_prioridad = Combobox(frame_crear_ticket,values=lista_prioridad, state='readonly')
-entry_crear_ticket_hardware = Entry(frame_crear_ticket)
-combobox_crear_ticket_tecnico = Combobox(frame_crear_ticket, values=lista_tecnico, state='readonly')
+lista_label_crear_ticket = [
+    Label(frame_crear_ticket, text='Asunto *'),
+    Label(frame_crear_ticket, text='Área *'),
+    Label(frame_crear_ticket, text='Prioridad *'),
+    Label(frame_crear_ticket, text='Código Hardware  '),
+    Label(frame_crear_ticket, text='Técnico  '),
+    Label(frame_crear_ticket, text='Detalle *')
+]
 
 frame_text_crear_ticket = Frame(frame_crear_ticket)
-frame_text_crear_ticket.grid(column=0, row=6)
+frame_text_crear_ticket.columnconfigure(0, weight=1)
+
 text_crear_ticket_detalle = Text(frame_text_crear_ticket, height=5, width=0, font=fuente_label)
 crear_ticket_scrollbar = Scrollbar(frame_text_crear_ticket, command=text_crear_ticket_detalle.yview)
-frame_text_crear_ticket.columnconfigure(0, weight=1)
 text_crear_ticket_detalle.config(yscrollcommand=crear_ticket_scrollbar.set)
 text_crear_ticket_detalle.grid(column=0, row=0, sticky='nsew')
 crear_ticket_scrollbar.grid(row=0, column=1, sticky='ns')
 
-lista_label_crear_ticket = [label_crear_ticket_asunto, label_crear_ticket_area, label_crear_ticket_prioridad,
-                            label_crear_ticket_hardware, label_crear_ticket_tecnico, label_crear_ticket_detalle]
-lista_entry_crear_ticket = [entry_crear_ticket_asunto, combobox_crear_ticket_area, combobox_crear_ticket_prioridad,
-                            entry_crear_ticket_hardware, combobox_crear_ticket_tecnico, frame_text_crear_ticket]
-
-posicionar_formulario(lista_label_crear_ticket, lista_entry_crear_ticket)
+lista_entry_crear_ticket = [
+    Entry(frame_crear_ticket),
+    Combobox(frame_crear_ticket,values=lista_area, state='readonly'),
+    Combobox(frame_crear_ticket,values=lista_prioridad, state='readonly'),
+    Entry(frame_crear_ticket),
+    Combobox(frame_crear_ticket, values=lista_tecnico, state='readonly'),
+    frame_text_crear_ticket
+]
 
 button_crear_ticket = Button(frame_crear_ticket, text='Crear', command=crear_ticket, cursor='hand2')
-button_crear_ticket.grid(column=1, row=7, padx=24, ipadx=16, ipady=6, sticky='e')
+
+posicionar_formulario(lista_label_crear_ticket, lista_entry_crear_ticket, button_crear_ticket)
 
 notebook_contenido.add(frame_crear_ticket, text='Crear Ticket')
 
